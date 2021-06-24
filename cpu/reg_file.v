2c9bfb7c307c0a5feddd884a177e78ad0f01ba3c
heshads <tharindudissa18@gmail.com>
2021-06-24 13:43:53 +0530

    regfile

--- cpu/reg_file.v
+++ cpu/reg_file.v
@@ -2,12 +2,12 @@
 
 module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK, RESET);
 
-input [31:0] IN;  // 8 bit data input
-input [4:0] INADDRESS, OUT1ADDRESS, OUT2ADDRESS; // 3 bit data inputs
+input [31:0] IN;  // 32 bit data input
+input [4:0] INADDRESS, OUT1ADDRESS, OUT2ADDRESS; // 5 bit data inputs
 input WRITE, CLK, RESET; // 1 bit data inputs
-output [31:0] OUT1, OUT2; // 8 bit data outputs
+output [31:0] OUT1, OUT2; // 32 bit data outputs
 
-reg [31:0] REGISTERS [31:0]; // 8x8 register file
+reg [31:0] REGISTERS [31:0]; // 32 bit x 32 register file
 
 
 // TODO : set the time delay
@@ -19,7 +19,7 @@ begin
     // write input to the in address
     
     // TODO : set the time delay
-    // #1
+    #1
     
     if (WRITE == 1'b1)
     begin
@@ -34,10 +34,10 @@ always @ (*)
 begin
     if (RESET == 1'b1)
     begin
-        
+
         // TODO : set the time delay
         #2 
-        for (i = 0; i < 8; i++) //looping through register file and setting them to 0s
+        for (i = 0; i < 32; i++) //looping through register file and setting them to 0s
         begin
             REGISTERS[i] = 0;
         end   
