751b9cc7b59aa67aa7087dd11c7dbf567d8b9572
dtdinidu7 <dtdinidu7@gmail.com>
2021-08-01 23:10:35 +0530

    Updated the assembler
    Co-authored-by: Heshan Dissanayake <tharindudissa18@gmail.com>

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -1,7 +1,5 @@
 import sys
 import csv
-from itertools import chain
-
 
 # instruction sets grouped by the type
 inst_data = {}
@@ -11,15 +9,52 @@ argList = {'inp_file': '', 'out_file': ''}
 # arg keywords
 argKeys = {'-i': 'inp_file', '-o': 'out_file'}
 
-# function to format the text
-
+# array to load the instructions
+Instructions = []
 
 def format(numOfDigits, num):
     return str(num).zfill(numOfDigits)
 
+# instruction formatting structure
+# loop through the instructions and do the nessary stuff
+def formatInstructions(Instructions):
+    for ins in Instructions:
+        formatInstruction(ins)
+
+# format the different types of instructions
+def formatInstruction(ins):
+    # final instruction
+    finalIns = []
+    tmp_split = ins.split()
+    instruction_name = tmp_split.pop(0)
+    finalIns.append(instruction_name.upper())
+    # spliting by the ','
+    for tmp in tmp_split:
+        # splitting by comma
+        tmp_split_2 = tmp.split(',')
+        tmp_split_2 = list(filter(lambda a: a != '', tmp_split_2))
+
+        # segmented list before putting together
+        segmented_list = []
+
+        for item in tmp_split_2:
+            # removing letter x 
+            item = item.replace('x', '')
+            # identifyng the sections with brackets
+            if '(' and ')' in item:
+                # removing ) from the string
+                item = item.replace(')', '')
+                tmp_split_3 = item.split('(')
+                segmented_list.extend(tmp_split_3)
+            else:
+                segmented_list.append(item)
+
+        finalIns.extend(segmented_list)
+
+    #print(instruction_name, finalIns)
+    handleInstruction(finalIns)
+
 # read the csv and create the instruction data dictionary
-
-
 def read_csv():
     # reading the csv file
     with open('RV32IM.csv') as csv_file:
@@ -34,28 +69,44 @@ def read_csv():
             inst_data[inst] = {'opcode': opcode,
                                'funct3': funct3, 'funct7': funct7, 'type': _type}
 
-# handling the separate assembly instructions
+# handling the separated instructions
+def handleInstruction(separatedIns):
+    Instruction = None
+    space = ' ' # used to visualize the space in instruction in debug mode
 
+    # handle R-Type instructions
+    if(inst_data[separatedIns[0]]['type'] == "R-Type"):
+        Instruction = inst_data[separatedIns[0]]['funct7'] + toBin(5, separatedIns[3]) + toBin(
+            5, separatedIns[2]) + inst_data[separatedIns[0]]['funct3'] + toBin(5, separatedIns[1]) + inst_data[separatedIns[0]]['opcode']
+        
+    elif(inst_data[separatedIns[0]]['type'] == "I - Type "):
+        Instruction = toBin(12, separatedIns[3]) + space + toBin(5, separatedIns[2]) + space + inst_data[separatedIns[0]]['funct3'] + space + toBin(5, separatedIns[1]) + space + inst_data[separatedIns[0]]['opcode']
+        
+        
+    elif(inst_data[separatedIns[0]]['type'] == "S-Type"):
+        # sw rs2:value, immediate, rs1:base
+        immediate = toBin(12, separatedIns[2])[::-1]
+        Instruction = immediate[5:] + space + toBin(5, separatedIns[1]) + space + toBin(5, separatedIns[3])+ space + inst_data[separatedIns[0]]['funct3']+ space + immediate[:5] + space + inst_data[separatedIns[0]]['opcode']
+    
+    elif(inst_data[separatedIns[0]]['type'] == "B-Type"):
+        # beq rs1, rs2, label
+        immediate = toBin(13, separatedIns[3])[::-1]
+        Instruction = immediate[12]+ space + immediate[5:10] + space + toBin(5, separatedIns[2])+ space + toBin(5, separatedIns[1]) + space + inst_data[separatedIns[0]]['funct3'] + space + immediate[1:4] + space + immediate[11] + space + inst_data[separatedIns[0]]['opcode'] 
+    
+    elif(inst_data[separatedIns[0]]['type'] == "U -Type"):
+        # lui rd, immediate
+        immediate = toBin(32, separatedIns[2])[::-1]
+        Instruction = immediate[12:31] + space + toBin(5, separatedIns[1]) + space + inst_data[separatedIns[0]]['opcode']
 
-def handleInstruction(Instruction):
-    separatedIns = list(map(lambda a: a.strip(), Instruction.split(',')))
-    # converting to the instrction to upper case
-    separatedIns = list(map(lambda a: a.upper(), separatedIns))
-    if len(separatedIns) > 0:
-        separatedIns[0] = list(
-            map(lambda a: a.strip(), separatedIns[0].split()))
-        separatedIns = list(chain.from_iterable(separatedIns))
-        print(separatedIns)
+    elif(inst_data[separatedIns[0]]['type'] == "J-Type"):
+        #jal rd, immediate
+        immediate = toBin(21, separatedIns[2])[::-1]
+        Instruction = immediate[20] + immediate[1:10]+ immediate[11] + immediate[12:19] + space + toBin(5, separatedIns[1]) + space + inst_data[separatedIns[0]]['opcode']
+        
 
-        Instruction = None
-        # handle R-Type instructions
-        if(inst_data[separatedIns[0]]['type'] == "R-Type"):
-            Instruction = inst_data[separatedIns[0]]['funct7'] + toBin(5, separatedIns[3]) + toBin(
-                5, separatedIns[2]) + inst_data[separatedIns[0]]['funct3'] + toBin(5, separatedIns[1]) + inst_data[separatedIns[0]]['opcode']
-            print(Instruction)
+    print(separatedIns[0], Instruction)
+
 # taking the file name if passed as an argument
-
-
 def handleArgs():
     n = len(sys.argv)
     for i in range(1, n):
@@ -73,17 +124,17 @@ def handleInpFile():
     f = open(argList['inp_file'], "r")
     # loop through the file and handle the instrctions separately
     for ins in f:
-        handleInstruction(ins)
+        Instructions.append(ins)
+        # handleInstruction(ins)
+
+    # start the instruction formatting
+    formatInstructions(Instructions)
 
 # convert a given number to binary according to a given format
-
-
 def toBin(numOfDigits, num):
-    return format(numOfDigits, "{0:b}".format(num))
+    return format(numOfDigits, "{0:b}".format(int(num)))
 
 # saving data to a .bin file
-
-
 def saveToFile(line):
     file = argList['inp_file'].split('.')[0] + '.bin'
     if not (argList['out_file'] == ''):
@@ -95,6 +146,8 @@ def saveToFile(line):
 
 
 if __name__ == "__main__":
+    #create the instruction disctionary
+    read_csv()
     # handdle the argments
     handleArgs()
     # input file reding sequence
--- Assembler/temp.py
+++ Assembler/temp.py
@@ -1,23 +1,10 @@
-import csv
-inst_data = {}
 
-# function to format the text
-def format(numOfDigits, num):
-    return str(num).zfill(numOfDigits)
+a = '021010010010'
 
-# read the csv and create the instruction data dictionary 
-def read_csv():
-    # reading the csv file
-    with open('RV32IM.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        
-        for row in csv_reader:
-            opcode = format(7, row[0])
-            funct3 = format(3,row[1])
-            funct7 = format(7,row[2])
-            inst = row[3]
-            _type = row[4]
-            inst_data[inst] = {'opcode':opcode, 'funct3':funct3, 'funct7':funct7, 'type':_type}
 
-read_csv()
-print(inst_data)
+# Instruction = toBin(12, separatedIns[3]) + space + toBin(5, separatedIns[2]) + space + inst_data[separatedIns[0]]['funct3'] + space + toBin(5, separatedIns[1]) + space + inst_data[separatedIns[0]]['opcode']
+
+# immediate = toBin(12, separatedIns[3])
+immediate = '123456789'[::-1]
+Instruction = immediate[2:7]
+print(immediate)
